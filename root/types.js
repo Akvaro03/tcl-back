bff973468cbff5810be02628e12bc5c85de5f22c
Alvaro <68175898+Akvaro03@users.noreply.github.com>
2024-01-03 10:50:31 -0300

    tiempo estimado por defecto a fecha de entrega estimada en base a la actividad de mayor tiempo

--- root/types.js
+++ root/types.js
@@ -3,17 +3,14 @@ class OType {
     constructor({ Fecha_OT, OT_ID, Empresa, Producto, Marca, Modelo, Fecha_Estimada_Entrega, Cotizacion, Item1, Description1, Importe1, Item2, Description2, Importe2, Item3, Description3, Importe3, TOTAL, Observacion }) {
         this.Date = new Date(refactorDate(Fecha_OT)).getTime()
         this.FechaEstimada = Fecha_Estimada_Entrega
-        this.Description1 = Description1
-        this.Description2 = Description2
-        this.Description3 = Description3
+        this.Description = [{ item: Item1, Description: Description1, import: Importe1 },
+        { item: Item2, Description: Description2, import: Importe2 },
+        { item: Item3, Description: Description3, import: Importe3 }]
         this.Observations = Observacion
         this.Cotizacion = Cotizacion
         this.StateProcess = "End"
         this.FechaVencimiento = ""
         this.Producto = Producto
-        this.Importe1 = Importe1
-        this.Importe2 = Importe2
-        this.Importe3 = Importe3
         this.Availability = null
         this.NormaAplicar = ""
         this.Client = Empresa
@@ -21,10 +18,7 @@ class OType {
         this.Activities = ""
         this.Factura = null
         this.OTKey = OT_ID
-        this.Item1 = Item1
-        this.Item2 = Item2
         this.Marca = Marca
-        this.Item3 = Item3
         this.IdClient = ""
         this.Contact = ""
         this.Changes = ""
@@ -38,7 +32,7 @@ class Client {
         this.KeyUnique = KeyUnique
         this.Contacts = ""
         this.Document = JSON.stringify({ type: TYPE_DOCUM, value: VALUE_DOCUM })
-        this.location = JSON.stringify({ direction: DIRECCION, province: PROVINCIA, location: LOCALIDAD })
+        this.location = `${DIRECCION}, ${PROVINCIA}, ${LOCALIDAD}`
     }
 }
 
@@ -46,4 +40,4 @@ const refactorDate = (date) => {
     const data = date.split("/")
     return `${data[1]}/${data[0]}/${data[2]}`
 }
-module.exports = {OType, Client} 
+module.exports = { OType, Client } 
